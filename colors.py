60be2bcf5b61205ccfe14b7d6af35cd236611bca
Albin Niva Printz <albin.printz@gmail.com>
2022-03-21 20:10:24 +0100

    Fix white and black detection, and massively decrease hue limit.

--- colors.py
+++ colors.py
@@ -9,7 +9,7 @@ WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 
 
-HUE_LIMIT = 2*math.pi/8
+HUE_LIMIT = math.pi / 10
 
 forbidden_color_groups = [
     [
@@ -23,7 +23,6 @@ forbidden_color_groups = [
         EncounteredColor(color=YELLOW, encountered=False),
     ],
 ]
-
 
 
 def ukraine() -> dict[int, Color]:
@@ -66,8 +65,22 @@ def should_replace_color(colors: dict[int, Color]) -> bool:
         for color in colors.values():
             found_forbidden = False
             for j, forbidden_color in enumerate(forbidden_color_group):
-                distance = color_distance(color, forbidden_color.color)
-                if distance < HUE_LIMIT:
+                distance = hue_distance(color, forbidden_color.color)
+
+                same_hue = distance < HUE_LIMIT
+                is_bright = [c > 200 for c in color]
+                is_dark = [c > 200 for c in color]
+                both_white = all(is_bright)
+                both_black = all(is_dark)
+                if (
+                    (forbidden_color.color == WHITE and both_white)
+                    or (forbidden_color.color == BLACK and both_black)
+                    or (
+                        forbidden_color.color != WHITE
+                        and forbidden_color.color != BLACK
+                        and same_hue
+                    )
+                ):
                     # We encountered one of the forbidden colors
                     forbidden_color_groups[i][j].encountered = True
                     found_forbidden = True
@@ -75,23 +88,35 @@ def should_replace_color(colors: dict[int, Color]) -> bool:
                 # Not this particular forbidden
                 found_not_forbidden = True
                 break
-        is_this_forbidden = (
-            all(
-                forbidden_color.encountered for forbidden_color in forbidden_color_groups[i]
-            )
-            and (not found_not_forbidden)
-        )
+        is_this_forbidden = all(
+            forbidden_color.encountered for forbidden_color in forbidden_color_groups[i]
+        ) and (not found_not_forbidden)
         if is_this_forbidden:
             return True
     return False
 
-def color_distance(a, b) -> float:
-    a_obj = colour.Color(rgb=(a[0]/255, a[1]/255, a[2]/255))
-    a_hue = a_obj.get_hue()*math.pi*2
-    b_obj = colour.Color(rgb=(b[0]/255, b[1]/255, b[2]/255))
-    b_hue = b_obj.get_hue()*math.pi*2
 
-    diff = abs(a_hue - b_hue) % (math.pi*2)
-    diff = diff if diff < math.pi else math.pi*2 - diff
+def verify_forbidden_colors():
+    for i, fcg in enumerate(forbidden_color_groups):
+        for j, c1 in enumerate(fcg):
+            for k, c2 in enumerate(fcg):
+                if j >= k:
+                    continue
+                d = hue_distance(c1.color, c2.color)
+                if d < HUE_LIMIT:
+                    print(f"{i} has a contradiction between {c1.color} and {c2.color}")
+
+
+def hue_distance(a, b) -> float:
+    a_obj = colour.Color(rgb=(a[0] / 255, a[1] / 255, a[2] / 255))
+    a_hue = a_obj.get_hue() * math.pi * 2
+    b_obj = colour.Color(rgb=(b[0] / 255, b[1] / 255, b[2] / 255))
+    b_hue = b_obj.get_hue() * math.pi * 2
+
+    diff = abs(a_hue - b_hue) % (math.pi * 2)
+    diff = diff if diff < math.pi else math.pi * 2 - diff
     return diff
 
+
+if __name__ == "__main__":
+    verify_forbidden_colors()
